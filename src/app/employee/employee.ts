56040b1296313c59eeadf6ffeb637110720dc672
Nuttarac <nuttara.c@grouplease.co.th>
2018-03-24 11:22:22 +0700

    make service and model

---
+++ src/app/employee/employee.ts
@@ -0,0 +1,22 @@
+export class Employee {
+    firstName: string;
+    lastName: string;
+    birthDate: Date;
+    address: string;
+    email: string;
+    contactNumber: string;
+    mobileNumber: string;
+    appliedPosition : string;
+    expectedSalary: number;
+    reference: string;
+    degree: string;
+    university: string;
+    major: string;
+    gpa: number;
+    companyname: string;
+    startDate: string;
+    endDate: string;
+    role: string;
+    responsibility: string;
+  }
+  
